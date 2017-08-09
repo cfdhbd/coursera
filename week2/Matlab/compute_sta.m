06b84fe04ca83887bfffc73ecbdc91520a94a2ea
cfdhbd <jonathankirchman@Jonathans-MacBook-Air-2.local>
2017-08-09 13:19:19 -0500

    week 2 quiz complete!

---
+++ week2/Matlab/compute_sta.m
@@ -0,0 +1,38 @@
+function [ sta,spike_times,window ] = compute_sta( stim, rho, num_timesteps, num_obv_spikes )
+%COMPUTE_STA Calculates the spike-triggered average for a neuron that
+%            is driven by a stimulus defined in stim. The spike-
+%            triggered average is computed over num_timesteps timesteps.
+    sta = zeros(num_timesteps, 1);
+
+    % This command finds the indices of all of the spikes that occur
+    % after 300 ms into the recording.
+    spike_times = find(rho(num_timesteps+1:end)) + num_timesteps;
+
+    % Fill in this value. Note that you should not count spikes that occur
+    % before 300 ms into the recording.
+    num_spikes = num_obv_spikes;
+    
+     for t = 1:num_spikes
+     window(t,:) = stim(((spike_times(t,1)-num_timesteps):spike_times(t,1)),1);
+     end
+     
+     for a = 1:num_timesteps
+         sta(a,1) = sum(window(:,a),1)/num_obv_spikes;
+     end
+     
+    %window =1;
+    %window(1,:) = stim(((spike_times(1,1)-150):spike_times(1,1)),1);
+    
+    
+    % Compute the spike-triggered average of the spikes found using the
+    % find command. To do this, compute the average of all of the vectors
+    % starting 300 ms (exclusive) before a spike and ending at the time of
+    % the event (inclusive). Each of these vectors defines a list of
+    % samples that is contained within a window of 300 ms before the each
+    % spike. The average of these vectors should be completed in an
+    % element-wise manner.
+    % 
+    % Your code goes here.
+
+end
+
