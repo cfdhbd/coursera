d7c95b1056b0a1c0ac80d68f50f0789be40bdac5
cfdhbd <jonathankirchman@Jonathans-MacBook-Air-2.local>
2017-08-10 15:05:11 -0500

    converting matlab to py

--- week2/Matlab/quiz2.m
+++ week2/Matlab/quiz2.m
@@ -6,7 +6,7 @@ sampling_period = 2; % in ms
 num_timesteps = 150;
 prior = 0;
 
-for n = 1:150
+for n = 1:num_timesteps
 val = rho(n,1);
 prior = prior + val;
 end
@@ -16,7 +16,7 @@ num_obv_spikes = sum(rho) - prior;
 [sta,spike_times,window] = compute_sta(stim, rho, num_timesteps, num_obv_spikes);
 
 time = -sampling_period*(num_timesteps-1):sampling_period:0; % in ms
-%spikes = count(1,time);
+
 figure(1);
 plot(time, sta);
 xlabel('Time (ms)');
